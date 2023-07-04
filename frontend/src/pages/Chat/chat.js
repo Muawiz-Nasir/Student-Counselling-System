74768f1e69b1a5a8f8643af2cd97c43d1809d805
Muawiz Nasir <muawiznasir99shine@gmail.com>
2023-07-05 00:58:31 +0500

    Chatbot

--- frontend/src/pages/Chat/chat.js
+++ frontend/src/pages/Chat/chat.js
@@ -2,7 +2,52 @@ import './chat.css';
 
 const Chat = () => {
     return <div className="chat-container">
-        <div className="chat-header">
+          <div className="new-chat-button">
+                <button>Add New Chat</button>
+            </div>
+        <div className="previous-chats">
+            <div className="chat">
+                <div className="avatar"></div>
+                <div className="chat-details">
+                    <h3>John Doe</h3>
+                    <p>Last message received...</p>
+                </div>
+            </div>
+            <div className="chat active">
+                <div className="avatar"></div>
+                <div className="chat-details">
+                    <h3>Jane Smith</h3>
+                    <p>Last message sent...</p>
+                </div>
+            </div>
+            {/* <!-- More previous chats --> */}
+        </div>
+        <div className="opened-chat">
+            <div className="chat-header">
+                <h2>Jane Smith</h2>
+            </div>
+            <div className="chat-body">
+                <div className="message received">
+                    <p>Hello! How can I assist you today?</p>
+                </div>
+                <div className="message sent">
+                    <p>I have a question about the project.</p>
+                </div>
+                {/* <!-- More chat messages --> */}
+            </div>
+            <div className="chat-input">
+                <input type="text" placeholder="Type your message..." />
+                <button>Send</button>
+            </div>
+        </div>
+
+    </div>
+
+    // </div>
+}
+
+export default Chat;
+{/* <div className="chat-header">
             <h2>Chat</h2>
         </div>
         <div className="chat-body">
@@ -16,12 +61,9 @@ const Chat = () => {
                 <p>Sure, I'd be happy to help. What specifically do you need assistance with?</p>
             </div>
             {/* <!-- More messages --> */}
-        </div>
+{/* </div>
         <div className="chat-input">
             <input type="text" placeholder="Type your message..." />
             <button>Send</button>
-        </div>
-    </div>
-}
-
-export default Chat;
+        // </div> */}
+{/* <div className="chat-container"> */ }
