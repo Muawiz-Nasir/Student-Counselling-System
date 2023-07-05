af2a43ee5957811632b13dc1cfffa8ccc4f50f2f
Muawiz-Nasir <muawiznasir99shine@gmail.com>
2023-07-05 18:43:37 +0500

    changes

--- frontend/src/pages/Chat/chat.js
+++ frontend/src/pages/Chat/chat.js
@@ -1,42 +1,77 @@
+import { useState } from 'react';
 import AuthenticatedStudentPagesLayout from '../../components/AuthenticatedStudentPagesLayout';
 import './chat.css';
 
 const Chat = () => {
+    const [selectedChat, setSelectedChat] = useState(null);
+
+    const chats = [{
+        id: 1,
+        title: "Helloo",
+        messages: [{
+            message: "hello",
+            sentBy: '123'
+        },
+        {
+            message: "hello",
+            sentBy: '1'
+        }
+    ]
+    },
+
+    {
+        id: 12,
+        title: "Helloo 2",
+        messages: [{
+            message: "hello2",
+            sentBy: '1'
+        },
+        {
+            message: "hello gjh",
+            sentBy: '1'
+        }
+    ]
+    }
+];
+
     // return <AuthenticatedStudentPagesLayout>
        return <div className="chat-container">
-          <div className="new-chat-button">
-                <button>Add New Chat</button>
-            </div>
         <div className="previous-chats">
-            <div className="chat">
-                <div className="avatar"></div>
-                <div className="chat-details">
-                    <h3>John Doe</h3>
-                    <p>Last message received...</p>
-                </div>
+        <div className="new-chat-button">
+                <button onClick={() => setSelectedChat(null)}>Add New Chat</button>
             </div>
-            <div className="chat active">
-                <div className="avatar"></div>
-                <div className="chat-details">
-                    <h3>Jane Smith</h3>
-                    <p>Last message sent...</p>
+            {
+                chats.map((chat) => {
+                    return <div className="chat" onClick={() => setSelectedChat(chat)}>
+                    <div className="avatar"></div>
+                    <div className="chat-details">
+                        <p>{chat.title}</p>
+                    </div>
                 </div>
-            </div>
-            {/* <!-- More previous chats --> */}
+                })
+            }
+
         </div>
         <div className="opened-chat">
-            <div className="chat-header">
-                <h2>Jane Smith</h2>
+           {
+            selectedChat &&  <div className="chat-body">
+            {/* <div className="message received">
+                <p>Hello! How can I assist you today?</p>
             </div>
-            <div className="chat-body">
-                <div className="message received">
-                    <p>Hello! How can I assist you today?</p>
-                </div>
-                <div className="message sent">
-                    <p>I have a question about the project.</p>
-                </div>
-                {/* <!-- More chat messages --> */}
+            <div className="message sent">
+                <p>I have a question about the project.</p>
             </div>
+            <div className="message sent">
+                <p>I have a question about the project.</p>
+            </div> */}
+
+            {
+                selectedChat.messages.map(message => <div className="message received">
+                <p>{message.message}</p>
+            </div>)
+            }
+        </div>
+           }
             <div className="chat-input">
                 <input type="text" placeholder="Type your message..." />
                 <button>Send</button>
